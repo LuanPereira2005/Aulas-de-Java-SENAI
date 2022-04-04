cfde6ca7bcaaeb3c5a111891ada095f1e23d202a
aluno <aluno@DSN1251017042>
2022-04-04 09:55:17 -0300

    Segundo Commit: Alteração da Mensagem

--- Teste_GH/src/Teste.java
+++ Teste_GH/src/Teste.java
@@ -3,7 +3,7 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Pra galera debochar legal, ao som do cabeça de gelo. Olha a pedra!");
+		System.out.println("KAAAASINO!");
 
 	}
 
