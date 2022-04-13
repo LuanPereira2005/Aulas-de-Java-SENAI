aa414c84bbe91c98fa6537cbb5fc77dd2fc323c9
aluno <aluno@DSN1251017042>
2022-04-13 11:12:32 -0300

    Colocando o dar e usando a função IF

--- OrientaçãoaObjetos/src/Main.java
+++ OrientaçãoaObjetos/src/Main.java
@@ -3,16 +3,35 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		int fig_d;
+		
 		Pessoas pessoa1 = new Pessoas();
 		
 		pessoa1.nome = "Cleito Rasta";
-		pessoa1.num_fig = 5;
-		
-		pessoa1.receber(5);
+		pessoa1.num_fig = 250;
+		fig_d = 200;
 		
 		System.out.println(pessoa1.nome);
 		System.out.println(pessoa1.num_fig);
-
+		
+		Pessoas pessoa2 = new Pessoas();
+		
+		pessoa2.nome = "Dj Azeitona";
+		pessoa2.num_fig = 150;
+		
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.num_fig);
+		
+		boolean msg = pessoa1.dar(200,pessoa2);
+		if(msg==true) {
+			System.out.println(pessoa1.nome);
+			System.out.println(pessoa1.num_fig);
+			System.out.println(pessoa2.nome);
+			System.out.println(pessoa2.num_fig);
+		}
+			
+		}
 	}
+	
 
-}
+
