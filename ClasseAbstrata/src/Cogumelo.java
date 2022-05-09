bc9cd7f8044b5ec38a08f2e1b8ec423e72011c96
aluno <aluno@DSN1251017042>
2022-05-09 11:43:01 -0300

    Commit final 09/05

---
+++ ClasseAbstrata/src/Cogumelo.java
@@ -0,0 +1,10 @@
+
+public class Cogumelo extends Item{
+
+	@Override
+	public void pegar() {
+		System.out.println("Pegando um cogumelo e ficando grandão");
+	}
+
+	
+}
---
+++ ClasseAbstrata/src/Moeda.java
@@ -0,0 +1,11 @@
+
+public class Moeda extends Item{
+
+	@Override
+	public void pegar() {
+		System.out.println("Pegando uma moeda e continuando pobretão");
+	}
+	
+
+
+}
