a25017d2496a310433c9ef16d48161c81a0a8838
aluno <aluno@DSN1251017042>
2022-05-09 08:12:46 -0300

    Polimorfismo

---
+++ Polimorfismo/src/Aplicacao.java
@@ -0,0 +1,25 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Animal animal = new Cachorro();
+		
+		animal.falar();
+		
+		animal = new Gato();
+		
+		animal.falar();
+		
+		
+//		Cachorro caramelo = new Cachorro();
+//		Gato anubis = new Gato();
+//		
+//		caramelo.falar();
+//		caramelo.morder();
+//		
+//		anubis.falar();
+		
+	}
+
+}
