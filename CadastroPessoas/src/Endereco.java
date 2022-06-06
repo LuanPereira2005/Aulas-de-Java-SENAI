ae5e232760268cdffbea9171dec339f1b0f25ddf
LuanPereira2005 <102962815+LuanPereira2005@users.noreply.github.com>
2022-06-06 11:35:37 -0300

    Add files via upload

--- CadastroPessoas/src/Endereco.java
+++ CadastroPessoas/src/Endereco.java
@@ -1,39 +1,47 @@
-
 public class Endereco {
-	 String rua;
-	 String nº;
-	 String bairro;
-	
-	 
+	// Criando os atributos de Endereco.
+	String rua;
+	String nº;
+	String bairro;
+
+	// Construtor de Endereco vazio.
 	public Endereco() {
-		
+
 	}
+
+	// Construtor de Endereco que usa um parametro do tipo String.
+	// Esse construtor faz o tratamento do parametro do tipo String.
 	public Endereco(String texto) {
 		String[] arrayAuxiliar1 = texto.split(",");
 		String[] arrayAuxiliar2 = arrayAuxiliar1[0].split(":");
 		this.rua = arrayAuxiliar2[1].trim();
-		
+
 		String[] arrayAuxiliar3 = arrayAuxiliar1[1].split(":");
 		this.nº = arrayAuxiliar3[1].trim();
-		
+
 		String[] arrayAuxiliar4 = arrayAuxiliar1[1].split(":");
 		this.bairro = arrayAuxiliar4[1].trim();
 	}
+	//Getters and Setters
 	public String setRua(String rua) {
 		this.rua = rua;
 		return rua;
 	}
+
 	public String setNº(String nº) {
 		this.nº = nº;
 		return nº;
 	}
+
 	public String setBairro(String bairro) {
 		this.bairro = bairro;
 		return bairro;
 	}
+
 	@Override
+	//Sobreposicao do metodo toString da classe Object.
 	public String toString() {
-		return "que mora na rua:"+rua+", que tem de numero da casa:"+nº+",que mora no bairro:"+bairro;
+		return "que mora na rua:" + rua + ", que tem de numero da casa:" + nº + ",que mora no bairro:" + bairro;
 	}
-	
+
 }
--- CadastroPessoas/src/Pessoa.java
+++ CadastroPessoas/src/Pessoa.java
@@ -1,31 +1,32 @@
 
 public class Pessoa {
+	// Criando os atributos de Endereco
 	private String nome;
 	private String idade;
 	private Sexo sexo;
 	private Endereco endereco;
 
-	
-	public Pessoa() {	
+	// Construtor de Pessoa vazio
+	public Pessoa() {
 	}
+	// Construtor de Pessoa que usa um parametro do tipo String.
+	// Esse construtor faz o tratamento do parametro do tipo String.
 	public Pessoa(String texto) {
 		String[] arrayAuxiliar1 = texto.split(",");
 		String[] arrayAuxiliar2 = arrayAuxiliar1[0].split(":");
 		this.nome = arrayAuxiliar2[1].trim();
-		
+
 		String[] arrayAuxiliar3 = arrayAuxiliar1[1].split(":");
 		this.idade = arrayAuxiliar3[1].trim();
-		
+
 		String[] arrayAuxiliar4 = arrayAuxiliar1[1].split(":");
-		if(arrayAuxiliar4[1]=="MASCULINO") {
+		if (arrayAuxiliar4[1] == "MASCULINO") {
 			this.sexo = Sexo.MASCULINO;
-		}
-		else {
+		} else {
 			this.sexo = Sexo.FEMININO;
 		}
 	}
-		
-
+	//Getters and Setters
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
@@ -37,18 +38,20 @@ public class Pessoa {
 	public void setSexo(Sexo sexo) {
 		this.sexo = sexo;
 	}
+
 	public Endereco getEndereço() {
 		return endereco;
 	}
 
-	public void setEndereço(Endereco endereço){
+	public void setEndereço(Endereco endereço) {
 		this.endereco = endereço;
 	}
 
 	@Override
+	//Sobreposicao do metodo toString da classe Object.
 	public String toString() {
-		return "Esse é o cadastro da pessoa do nome:"+ nome + ", e que tem a idade:"+ idade +", que é do sexo:" + sexo + endereco;
+		return "Esse é o cadastro da pessoa do nome:" + nome + ", e que tem a idade:" + idade + ", que é do sexo:"
+				+ sexo + endereco;
 	}
-	
-	
+
 }
