11a58f0f4a6c72ee158de04dce8445af26e0dde4
LuanPereira2005 <102962815+LuanPereira2005@users.noreply.github.com>
2022-06-06 09:48:34 -0300

    Add files via upload

--- Exercicio 18.05.22/src/Main.java
+++ Exercicio 18.05.22/src/Main.java
@@ -1,16 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		List<Pessoa>listaDePessoas=new ArrayList<Pessoa>();
+		List<Pessoa>listaDePessoas2=new ArrayList<Pessoa>();
 		Scanner entrada = new Scanner(System.in);
-		
+		int Menu=0;
 		Sexo sexo = null;
-		int s = 0;
 		
 		for(int x = 0; x<2 ; x++) {
 		Pessoa pessoa1 = new Pessoa();
@@ -19,15 +24,11 @@ public class Main {
 		pessoa1.setNome(entrada.next());
 		System.out.println("Digite a sua idade");
 		pessoa1.setIdade(entrada.nextInt());
-		System.out.println("Digite o nome da sua rua");
-		pessoa1.getEndereço().setBairro(entrada.next());
-		System.out.println("Digite o nome do seu bairro");
-		pessoa1.getEndereço().setBairro(entrada.next());
-		System.out.println("Digite o número da sua casa");
-		pessoa1.getEndereço().setNº(entrada.nextInt());
 		System.out.println("Selecione o seu sexo");
 		System.out.println("1-Masculino");
 		System.out.println("2-Feminino");
+		int s = 0;
+		while((s!=1) && (s!=2)) {
 		s=entrada.nextInt();
 			if(s==1) {
 				sexo = Sexo.MASCULINO;
@@ -36,14 +37,46 @@ public class Main {
 				sexo = Sexo.FEMININO;
 				pessoa1.setSexo(sexo);
 			}
-
-	
+			else {
+				System.out.println("Selecionou um sexo inexistente");
+				System.out.println("Selecione o seu sexo");
+				System.out.println("1-Masculino");
+				System.out.println("2-Feminino");
+			}
+		}
+		System.out.println("Digite o nome da sua rua");
+		pessoa1.getEndereço().setRua(entrada.next());
+		System.out.println("Digite o nome do seu bairro");
+		pessoa1.getEndereço().setBairro(entrada.next());
+		System.out.println("Digite o número da sua casa");
+		pessoa1.getEndereço().setNº(entrada.next());
 		listaDePessoas.add(pessoa1);
-		
+		System.out.println("Selecione o que você quer fazer:");
+		System.out.println("1-Salvar");
+		System.out.println("2-Visualizar");
+		System.out.println("3-Atualizar");
+		System.out.println("4-Deletar");
 		}	
 		for(Pessoa pessoa: listaDePessoas) {
 			System.out.println(pessoa);
 			}
+		
+		try(BufferedWriter escrever = new BufferedWriter(new FileWriter("Saída.txt",true))){
+			for(Pessoa pessoa: listaDePessoas) {
+				escrever.write(pessoa.toString()+ "\n");
 	}
-
+	}
+		try (BufferedReader reader = new BufferedReader(new FileReader("Saída.txt"))) {
+			String line;
+			
+			while((line = reader.readLine())!=null) {
+				
+				Pessoa pessoa = new Pessoa(line);
+				listaDePessoas2.add(pessoa);
+			}
+		}
+		for(Pessoa pessoa: listaDePessoas2) {
+			System.out.println(pessoa);
+		}
+}
 }
--- Exercicio 18.05.22/src/Pessoa.java
+++ Exercicio 18.05.22/src/Pessoa.java
@@ -4,9 +4,35 @@ public class Pessoa {
 	private int idade;
 	private Sexo sexo;
 	private Endereco endereço;
-	private static int y;
+	
+	
+	public Pessoa() {
+	}
+	public Pessoa(String texto) {
+		String[] arrayAuxiliar1 = texto.split(",");
+		String[] arrayAuxiliar2 = arrayAuxiliar1[0].split(":");
+		this.nome = arrayAuxiliar2[1].trim();
+		
+		String[] arrayAuxiliar3 = arrayAuxiliar1[1].split(":");
+		this.idade = Integer.parseInt(arrayAuxiliar3[1].trim());
+		
+		String[] arrayAuxiliar4 = arrayAuxiliar1[2].split(":");
+		this.sexo = Sexo.valueOf(arrayAuxiliar4[1].trim());
+		
+//		if("FEMININO".equals(arrayAuxiliar4[1].trim()) ) {
+//			this.sexo = Sexo.FEMININO;
+//		}
+//		else if("MASCULINO".equals(arrayAuxiliar4[1].trim()) ){
+//			this.sexo = Sexo.MASCULINO;
+//		}else {
+//		}
+
+		Endereco endereço1 = new Endereco(texto);
+		this.endereço = endereço1;
+	}
 
 
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
@@ -25,12 +51,13 @@ public class Pessoa {
 	public void setEndereço(Endereco endereço){
 		this.endereço = endereço;
 	}
-
+	
 	@Override
 	public String toString() {
-		y++;
-		return "Esse é o cadastro da pessoa do número: " + y + " nome:"+ nome + " que tem a idade:"+ idade +" do sexo:" + sexo + " que mora na rua:"+ endereço.rua +" no número:"+ endereço.nº +" no bairro:"+ endereço.bairro;	
+		return "Esse é o cadastro da pessoa de nome: "+ nome + ", que tem a idade: "+ idade +",do sexo:" + sexo + endereço.toString();
 	}
+
+	
 
 
 	
