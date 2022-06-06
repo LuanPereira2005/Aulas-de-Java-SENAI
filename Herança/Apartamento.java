488bb2db4de87ff3a77b791a2868cec5302b4692
LuanPereira2005 <102962815+LuanPereira2005@users.noreply.github.com>
2022-06-06 09:58:48 -0300

    Add files via upload

---
+++ Herança/Apartamento.java
@@ -0,0 +1,9 @@
+
+public class Apartamento implements ItemCaro{
+	
+	@Override
+	public String getPreco() {
+		return "$2500000";
+	}
+
+}
---
+++ Sobrecarga/ExercicioData/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Data data1= new Data(27,12,2005);
+		
+
+		System.out.println(data1.imprimir());
+			
+	}
+
+}
