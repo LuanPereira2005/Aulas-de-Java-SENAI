95f395f6b1e5d95e7eb08a9d798ca42f0253beb4
aluno <aluno@DSN1251017045>
2022-06-07 09:49:21 -0300

    Colocando comentários da atividade do professor Rafael

--- CadastroPessoas/src/Sexo.java
+++ CadastroPessoas/src/Sexo.java
@@ -1,5 +1,4 @@
 //Criacao de um enum
-//O enum e um tipo de dado utilizado para armazenar um conjunto de valores constantes, ou seja, sao valores fixos, que nao podem ser modificados.
 public enum Sexo {
 	MASCULINO, FEMININO;
 }
--- Enum/src/DiaDaSemana.java
+++ Enum/src/DiaDaSemana.java
@@ -1,22 +1,19 @@
+// Criacao de um enum
+// O enum e um tipo de dado utilizado para armazenar um conjunto de valores constantes, ou seja, sao valores fixos, que nao podem ser modificados.
 public enum DiaDaSemana {
-	DOMINGO(1),
-	SEGUNDA(2),
-	TERCA(3),
-	QUARTA(4),
-	QUINTA(5),
-	SEXTA(6),
-	SABADO(7);
+	DOMINGO(1), SEGUNDA(2), TERCA(3), QUARTA(4), QUINTA(5), SEXTA(6), SABADO(7);
 	
+	// Criacao de um atributo int
 	private int num;
 
+	// Construtor de DiaDaSemana com um parametro int
 	private DiaDaSemana(int num) {
 		this.num = num;
 	}
-
+	
+	// Get
 	public int getNum() {
 		return num;
 	}
-	
-	
-	
+
 }
