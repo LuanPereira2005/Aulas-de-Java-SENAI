09ca1145647bb79c714e3cb625c2f8d6a3bdf4ec
aluno <aluno@DSN1251017045>
2022-06-07 08:24:59 -0300

    Colocando comentários da atividade do professor Rafael

--- ClasseAbstrata/src/Moeda.java
+++ ClasseAbstrata/src/Moeda.java
@@ -1,7 +1,7 @@
 
 public class Moeda extends Item{
 	
-
+	//Subscrita do metódo pegar da classe abstrata Item
 	@Override
 	public void pegar() {
 		System.out.println("Pegando uma moeda e continuando pobretão");
