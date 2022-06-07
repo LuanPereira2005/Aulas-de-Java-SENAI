f233803575bebfcc4bcc957c230ae09727dd47ce
aluno <aluno@DSN1251017045>
2022-06-07 08:39:04 -0300

    Colocando comentários da atividade do professor Rafael

--- ContaBancária/src/ContaCorrente.java
+++ ContaBancária/src/ContaCorrente.java
@@ -1,10 +1,10 @@
-
+// Criacao da classe ContaCorrente que extende da classe abstrata ContaBancaria.
 public class ContaCorrente extends ContaBancaria {
-	
-	// Sobrescrita do metodo abstrato calcularSaldo
+
+	// Sobrescrita do metodo abstrato calcularSaldo.
 	@Override
 	public String calcularSaldo() {
-		saldo -= saldo*10/100;
+		saldo -= saldo * 10 / 100;
 		return "O seu saldo final é:" + saldo;
 	}
 
--- ContaBancária/src/ContaInvestimento.java
+++ ContaBancária/src/ContaInvestimento.java
@@ -1,7 +1,7 @@
-
+// Criacao da classe ContaCorrente que extende da classe abstrata ContaBancaria.
 public class ContaInvestimento extends ContaBancaria {
 	
-	// Sobrescrita do metodo abstrato calcularSaldo
+	// Sobrescrita do metodo abstrato calcularSaldo.
 	@Override
 	public String calcularSaldo() {
 		saldo += saldo * 5 / 100;
